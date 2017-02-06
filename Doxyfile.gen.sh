f114218397a1e78c8bbe2b3cc0f98bafb1687cab
Emanuel Palm <emanuelpalm@gmail.com>
2017-02-06 20:06:11 +0100

    Make Doxyfile generated via "make dox"

---
+++ Doxyfile.gen.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Script for generating file.
+TARGET=Doxyfile.gen
+
+command -v doxygen >/dev/null 2>&1 || {
+    echo >&2 "Doxygen not available in PATH.";
+    exit 1;
+}
+
+command -v git >/dev/null 2>&1 && {
+    GIT_TAG=$(git describe --first-parent 2> /dev/null)
+    PROJECT_NUMBER=$(expr "$GIT_TAG" : 'v\([0-9]\+.[0-9]\+.[0-9]\+\)')
+}
+
+echo -n "\
+DOXYFILE_ENCODING      = UTF-8
+PROJECT_NAME           = pac
+PROJECT_NUMBER         = $PROJECT_NUMBER
+PROJECT_BRIEF          =
+PROJECT_LOGO           =
+OUTPUT_DIRECTORY       = dox/
+OPTIMIZE_OUTPUT_FOR_C  = YES
+JAVADOC_AUTOBRIEF      = YES
+INPUT                  = source/
+INPUT_ENCODING         = UTF-8
+FILE_PATTERNS          = *.h *.md
+RECURSIVE              = YES
+" > $TARGET
