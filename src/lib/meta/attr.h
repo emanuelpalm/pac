c999bc7c046c8d5885e2dcda56eddbf78c1f9422
Emanuel Palm <emanuelpalm@gmail.com>
2017-04-07 07:23:44 +0200

    Add src/lib/meta/attr.h

---
+++ src/lib/meta/attr.h
@@ -0,0 +1,48 @@
+#ifndef LIB_META_ATTR_H
+#define LIB_META_ATTR_H
+
+/// Function attributes.
+///
+/// This files contains various macros that may be invoked at the end of
+/// function prototypes to indicate special properties. The attributes will
+/// only be honored on supported platforms.
+///
+/// \file
+
+#if __GNUC__ >= 3
+
+#define ATTR_NORETURN() \
+    __attribute__((__noreturn__))
+
+#define ATTR_PRINTF(fmt, va_args) \
+    __attribute__((format (printf, (fmt), (va_args))))
+
+#else
+
+/// Indicates that a function never returns after being called.
+///
+/// ## Example
+///
+/// ```c
+/// void panic(const char *message)
+///     ATTR_NORETURN();
+/// ```
+#define ATTR_NORETURN()
+
+/// Indicates that a function takes a format string and format arguments
+/// compatible with those accepted by printf().
+///
+/// ## Example
+///
+/// ```c
+/// void log_printf(int logLevel, const char *format, ...)
+///     ATTR_PRINTF(2, 3);
+/// ```
+///
+/// \param fmt     The position of the format argument, starting from 1.
+/// \param va_args The position of the variadic argument, starting from 1.
+#define ATTR_PRINTF(fmt, va_args)
+
+#endif
+
+#endif
