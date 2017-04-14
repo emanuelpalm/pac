ec370e3ed4eb57734e1dfabffc06d7d308654e47
Emanuel Palm <emanuel.palm@gmail.com>
2017-04-14 13:11:25 +0200

    Add NULL assert to mem_newString()

--- src/lib/mem/string.h
+++ src/lib/mem/string.h
@@ -5,16 +5,19 @@
 ///
 /// \file
 
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
 /// Creates new string by copying given string.
 ///
-/// The provided string must be 0-terminated, or the function will cause
-/// undefined behavior.
+/// The provided string must not be NULL, and must be 0-terminated, or the
+/// function will cause undefined behavior.
 ///
 /// The returned string must be freed using free().
 static inline char *mem_newString(const char *string) {
+    assert(string != NULL);
+
     char *buf = NULL;
     const size_t len = strlen(string) + 1;
     if ((buf = malloc(len)) != NULL) {
